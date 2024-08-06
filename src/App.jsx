d102dab8cb58e9b247aeba9f191f2bdf21c5d64d
adityakmishra <adityamishra1872@gmail.com>
2024-08-07 01:18:53 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -129,7 +129,7 @@ function App() {
 
   const clear = () => {
     ctx.clearRect(0, 0, canvasRef.current.width, canvasRef.current.height);
-    ctx.fillStyle = bgcolr;
+    ctx.fillStyle = "#FFFFFF";
     ctx.fillRect(0, 0, canvasRef.current.width, canvasRef.current.height);
   };
 

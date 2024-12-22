0c39b41461e397168ecb9c45065b3002f40b8730
Aditya Kumar Mishra <115264757+AdityaKMishra01@users.noreply.github.com>
2024-12-22 11:55:14 +0530

    Update Links.jsx

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -10,7 +10,7 @@ const links = () => {
             <a href="#"><i class="fa-brands fa-discord"></i></a>
         </div>
         <div className="link2">
-            <a href="https://www.instagram.com/_imadityaa/" target='blank'><i class="fa-brands fa-instagram"></i></a>
+            <a href="#" target='blank'><i class="fa-brands fa-instagram"></i></a>
         </div>
         <div className="link3">
             <a href="https://github.com/AdityaKMishra01" target='blank'><i class="fa-brands fa-github"></i></a>
